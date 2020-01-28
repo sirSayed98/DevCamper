cc429d60da336c5e6af7004d67f8c636995cc3a9
mohamedsayed2011 <mohamed.sayed8899@gmail.com>
2020-01-28 13:38:13 +0200

    add files

---
+++ app.js
@@ -0,0 +1,6 @@
+console.log(' this is a maestro ');
+
+const fn = require('./functions.js')
+
+fn.multiblication(4,6)
+fn.sum(3,4);
---
+++ functions.js
@@ -0,0 +1,13 @@
+function add(a, b) {
+    console.log(a + b)
+}
+
+function mul(a, b) {
+    console.log(a * b)
+}
+
+
+module.exports = {
+    multiblication: mul,
+    sum: add
+}
