ad7c246f1165d198d1e0c8aa7ccaac4937375722
mohamedsayed2011 <mohamed.sayed8899@gmail.com>
2020-01-28 16:23:48 +0200

    add express framework .gitignore file

---
+++ server.js
@@ -0,0 +1,6 @@
+/*
+npm install express --save
+we use (--save) as we don't upload node_modules to githup so any one
+wantto use this project need to(npm install) so all dependencies in package.json
+will be downloaded
+ */
