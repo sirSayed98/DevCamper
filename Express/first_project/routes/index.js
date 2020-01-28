ac551161b31bb0fbdce6fd322e657bf0879aedcc
mohamedsayed2011 <mohamed.sayed8899@gmail.com>
2020-01-28 23:21:59 +0200

    deal with .jade

--- Express/first_project/routes/index.js
+++ Express/first_project/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'First project' });
+  res.render('index', { title: 'First project', condition:true });
 });
 
 module.exports = router;
