bcc77a467a55b51ca6a310afd992209c2e64077c
mohamedsayed2011 <mohamed.sayed8899@gmail.com>
2020-01-29 12:29:50 +0200

    dynamic get :id

--- Express/pro_hbs/routes/users.js
+++ Express/pro_hbs/routes/users.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
+router.get('/info/:id', function (req, res, next) {
+  res.render('info', { id: req.params.id });
+});
+
+
 
 module.exports = router;
