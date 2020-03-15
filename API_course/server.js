fe28ecc25cf20ffe4be5cbd24ee1190d2f656e4b
mohamedsayed2011 <mohamed.sayed8899@gmail.com>
2020-03-15 14:21:37 +0200

    connect to db

--- API_course/server.js
+++ API_course/server.js
@@ -1,12 +1,15 @@
 const express = require('express');//nodejs framework   
 const dotenv = require('dotenv');//for .env file
 const morgan = require('morgan');// for middleware
+const connectDB = require('./config/db')
+
 
 //load env variables
 dotenv.config({ path: './config/config.env' });
+// Connect to db
+connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
-
 
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
@@ -19,6 +22,15 @@ const bootcamps = require('./routes/bootcamps');
 
 app.use('/api/v1/bootcamps', bootcamps);
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`App is listening on port ${PORT}! in ${process.env.NODE_ENV} mode `);
 });
+
+//handle unhandled promise rejections
+process.on('unhandledRejections', (err, promise) => {
+    console.log('--------------------------------------------------');
+    console.log(`Eroor: ${err.message}`);
+    console.log('--------------------------------------------------');
+    //close server && exit process
+    server.close(() => process.exit(1));
+})
