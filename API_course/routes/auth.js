c481d1805399ef4521204e1aba731a0c519a4aa0
sirSayed98 <mohamed.sayed8899@gmail.com>
2020-04-19 23:16:59 +0200

    add login functionality

--- API_course/routes/auth.js
+++ API_course/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register } = require('../controllers/auth');
+const { register, login } = require('../controllers/auth');
 
 const router = express.Router();
 
@@ -7,4 +7,8 @@ const router = express.Router();
 router
     .route('/register')
     .post(register)
+
+router
+    .route('/login')
+    .post(login)
 module.exports = router;
