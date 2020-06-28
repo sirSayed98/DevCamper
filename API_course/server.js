a90cc17c6546edce2a4b20ab202a0cd6ba39f047
sirSayed98 <mohamed.sayed8899@gmail.com>
2020-06-28 22:35:05 +0200

    add admin functionality

--- API_course/server.js
+++ API_course/server.js
@@ -19,6 +19,8 @@ const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
 const auth = require('./routes/auth')
 const reviews= require('./routes/reviews')
+const users = require('./routes/users');
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -43,6 +45,7 @@ app.use(cookieParser());
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
 app.use('/api/v1/auth', auth);
+app.use('/api/v1/users', users);
 app.use('/api/v1/reviews', reviews);
 
 
