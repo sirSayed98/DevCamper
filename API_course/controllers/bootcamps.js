ab37543383f3a494bcd1f6d3967be358148838bc
mohamed sayed <mohamed.sayed8899@gmail.com>
2020-07-29 11:04:54 +0200

    change path of saving image

--- API_course/controllers/bootcamps.js
+++ API_course/controllers/bootcamps.js
@@ -201,7 +201,7 @@ exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
   // Create custom filename
   file.name = `photo_${bootcamp._id}${path.parse(file.name).ext}`;
 
-  file.mv(`${process.env.FILE_UPLOAD_PATH}/${file.name}`, async err => {
+  file.mv(`${__dirname}/client/public/uploads/${file.name}`, async err => {
     if (err) {
       console.error(err);
       return next(new ErrorResponse(`Problem with file upload`, 500));
